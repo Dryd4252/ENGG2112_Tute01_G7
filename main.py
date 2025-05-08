e64a3acd7dd1ae4e10fcf8b78b07efd7f08a28e5
Oliver Cook <mailolivercook@gmail.com>
2025-05-08 15:08:40 +1000

    Added some stuff to compare models and reduce copy pasting

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ import pandas as pd
 import ml_models
 
 import ml_models.mlp as mlp
+from ml_models.rfr import RfrModel as rfr
 
 def main():
-    seed = 6969
+    seed = 69420
     data = pd.read_csv("train.csv")
+    save_files = True
 
     mlp_model = mlp.MlpModel(data, 50, seed=seed)
-    mlp_model.process_data()
-    mlp_model.train_model()
-    mlp_model.make_prediction()
-    mlp_model.classify_model_performance()
-    print(mlp_model.get_statistics())
-    mlp_model.create_graph()
+    rfr_model = rfr(data, seed=seed)
 
+    ml_models = [mlp_model, rfr_model]
+    
+    for model in ml_models:
+        model.process_data()
+        model.train_model()
+        model.make_prediction()
+        model.classify_model_performance()
+        print(model.get_statistics())
+        model.create_graph(save_fig=save_files) # Saves graph if save_fig is True
+
+        if save_files: # Savees stats if save_files is True
+            model.save_statistics(model.name) 
 
 if __name__ == "__main__":
     main()
