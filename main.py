ff49f9ca7ff212e61d6f32e2070f4352a1f4c5a1
Imogen C <icow7529@uni.sydney.edu.au>
2025-05-22 12:25:48 +1000

    forgot main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import sys
 from scipy.stats import randint, uniform
+from sklearn.inspection import permutation_importance
+from packaging.version import parse as parse_version
 
 from ml_models.mlp import MlpModel as mlp
 from ml_models.rfr import RfrModel as rfr
 from ml_models.xgb import XgbModel as xgb
+from ml_models.xgb import plot_property_importance, plot_element_importance
+from ml_models.abstract_ml_model import plot_top_properties_boxplots
 
 def normalise(value, mean, std):
     z = (value - mean) / std
@@ -66,7 +71,7 @@ def rfr_test_optomiser(property_data: pd.DataFrame, seed: int):
     print(rfr_model_t.get_params())
 
 def xgb_optimiser(property_data: pd.DataFrame, seed: int):
-    param_grid = {
+    param_grid_test = {
         'n_estimators': randint(100, 500),     # 100 - 1000
         'learning_rate': uniform(0.01, 0.07),    #0.01 - 0.1
         'max_depth': randint(4, 7),           #3 - 10
@@ -74,6 +79,17 @@ def xgb_optimiser(property_data: pd.DataFrame, seed: int):
         'colsample_bytree': uniform(0.5, 0.5),   #0.5 - 1
         'lambda': uniform(1, 2),               #0 - > 1
         'alpha': uniform(1, 2),                #0 - > 1
+        'booster': ['gbtree']
+    }
+
+    param_grid = {
+        'n_estimators': [323],     # 100 - 1000
+        'learning_rate': [0.044994882],    #0.01 - 0.1
+        'max_depth': [4],           #3 - 10
+        'subsample': [0.500422911],          #0.5 - 1
+        'colsample_bytree': [0.55965652],   #0.5 - 1
+        'lambda': [2.333191548],               #0 - > 1
+        'alpha': [3.922654256],                #0 - > 1
         'booster': ['gbtree']
     }
 
@@ -122,44 +138,52 @@ def xgb_optimiser(property_data: pd.DataFrame, seed: int):
 def main(save_files):
     seed = None
     property_data = pd.read_csv("train.csv")
-    
+    # Sub-Problem A
+
+    print("Calling xgb_optimiser() for the first time to train model")
     xgb_model_t = xgb_optimiser(property_data, seed)
+    # plot_property_importance(xgb_model_t, property_data
+    plot_top_properties_boxplots(xgb_model_t, property_data)
     save_files = True
 
-    if save_files: # Savees stats if save_files is True
-        xgb_model_t.save_statistics(xgb_model_t.name) 
+    # for i in range(2):
+    #     xgb_model_t = xgb_optimiser(property_data, seed)
 
-    return
+    #     if save_files: # Savees stats if save_files is True
+    #         xgb_model_t.save_statistics(xgb_model_t.name) 
 
     # net_sizes = [(10,),(64,64),(128,64,32),(256,128,64)]
     # optimise_mlp(property_data,net_sizes,seed)
     
-    mlp_model_1 = mlp(property_data, 50, seed=seed)
-    rfr_model_1 = rfr(property_data, seed=seed)
-    xgb_model_1 = xgb(property_data, 100, 0.1, 5, seed=seed)
+    # mlp_model_1 = mlp(property_data, 50, seed=seed)
+    # rfr_model_1 = rfr(property_data, seed=seed)
+    # xgb_model_1 = xgb(property_data, 100, 0.1, 5, seed=seed)
 
-    ml_models_1 = [mlp_model_1, rfr_model_1, xgb_model_1]
+    # ml_models_1 = [xgb_model_1]
     
-    for model in ml_models_1:
-        model.process_data(["critical_temp"])
-        model.train_model()
-        model.test_prediction()
-        model.classify_model_performance()
-        print(model.get_statistics())
+    print("Saving data")
+    # for model in ml_models_1:
+    #     model.process_data(["critical_temp"])
+    #     model.train_model()
+    #     model.test_prediction()
+    #     model.classify_model_performance()
+    #     print(model.get_statistics())
 
-        model.create_graph(save_fig=save_files) # Saves graph if save_fig is True
-        if save_files: # Savees stats if save_files is True
-            model.save_statistics(model.name) 
+    #     model.create_graph(save_fig=save_files) # Saves graph if save_fig is True
+    #     if save_files: # Savees stats if save_files is True
+    #         model.save_statistics(model.name) 
 
-    xgb_model = xgb.XgbModel(property_data, 100, 0.1, 5, seed=seed)
-    xgb_model.process_data(["critical_temp"])
-    xgb_model.train_model()
-    xgb_model.make_prediction()
-    xgb_model.classify_model_performance()
-    print(xgb_model.get_statistics())
-    xgb_model.create_graph()
+    # xgb_model = xgb(property_data, 100, 0.1, 5, seed=seed)
+    # xgb_model.process_data(["critical_temp"])
+    # xgb_model.train_model()
+    # xgb_model.y_pred = xgb_model.make_prediction(xgb_model.X_test)
+    # xgb_model.classify_model_performance()
+    # print(xgb_model.get_statistics())
+    # xgb_model.create_graph()
+
     ### Sub Problem B
 
+    print("Beginning sub-problem B")
     symbol_data = pd.read_csv("unique_m.csv")
 
     symbol_drop_columns = ["critical_temp", "material"]
@@ -168,13 +192,28 @@ def main(save_files):
     subproblem_b_targets = symbol_data.copy()
 
     property_symbol_data = pd.concat([property_data, symbol_data], axis=1)
-    rfr_model_2_params = [sub_problem_B_target]
-    rfr_model_2 = rfr(property_symbol_data, seed=seed)
+    xgb_model_2_params = [subproblem_b_targets]
 
-    rfr_model_2.process_data(["critical_temp"])
-    rfr_model_2.train_model()
-    rfr_model_2.test_prediction()
-    rfr_model_2.classify_model_performance()
+    print("Training second model")
+    xgb_model_2 = xgb(property_symbol_data, seed=seed)
+    xgb_model_2.process_data(["critical_temp"])
+    print(xgb_model_2.X_train.columns)
+    xgb_model_2.train_model()
+    xgb_model_2.test_prediction()
+    xgb_model_2.classify_model_performance()
+    plot_element_importance(xgb_model_2, symbol_data)
+
+
+    # rfr_model_2_params = [sub_problem_B_target]
+    # rfr_model_2 = rfr(property_symbol_data, seed=seed)
+
+    # rfr_model_2.process_data(["critical_temp"])
+    # rfr_model_2.train_model()
+    # rfr_model_2.test_prediction()
+    # rfr_model_2.classify_model_performance()
+
+    print("Done")
+    plt.show()
 
 if __name__ == "__main__":
     save_files = len(sys.argv) > 1 and sys.argv[1] is True
