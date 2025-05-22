7a77249580db1efacf6cc4e5fc4d486b30607546
Timothy Shires <timothyshires@icloud.com>
2025-05-22 13:30:51 +1000

    Adding all parameters to MLP

--- ml_models/mlp.py
+++ ml_models/mlp.py
@@ -13,14 +13,43 @@ from . import abstract_ml_model
 
 class MlpModel(abstract_ml_model.AbstractMlModel):
     # im changing hl_sizes to a tuple to more easily change the variation of hidden layers, inputs will be lik (100,) or (128,64,32)
-    def __init__(self, data: pd.DataFrame, hl_sizes: tuple, activation = "relu",solver='adam', alpha=0.0001,learning_rate_init=0.001,early_stopping=False, max_iterations=10000, seed=None):
+    def __init__(self, 
+                 data: pd.DataFrame, 
+                 hl_sizes: tuple, 
+                 activation = "relu",
+                 solver='adam', 
+                 alpha=0.0001,
+                 batch_size="auto",
+                 learning_rate = "constant",
+                 learning_rate_init=0.001,
+                 max_iterations=10000, 
+                 tol = 1e-4,
+                 momentum = 0.9, 
+                 nesterovs_momentum = True,
+                 early_stopping=False, 
+                 validation_fraction = 0.1,
+                 beta_1 = 0.9,
+                 beta_2 = 0.999,
+                 epsilon = 1e-8,
+                 n_iter_no_change = 10,
+                 seed=None):
         super().__init__(data, self.__class__.__name__, seed=seed)
-        self.max_iterations: int =  max_iterations
         self.hl_sizes: tuple = hl_sizes 
         self.activation = activation
         self.solver = solver
         self.alpha = alpha 
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
         self.learning_rate_init = learning_rate_init
+        self.max_iterations: int =  max_iterations
+        self.tol = tol
+        self.momentum = momentum
+        self.nesterovs_momentum = nesterovs_momentum
+        self.validation_fraction = validation_fraction
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = epsilon
+        self.n_iter_no_change = n_iter_no_change
         self.early_stopping = early_stopping
 
     def process_data(self, target_label: list[str], split_size: float = 0.2,  exclude_features: list[str] = []) -> None:
@@ -47,8 +76,18 @@ class MlpModel(abstract_ml_model.AbstractMlModel):
                 activation=self.activation,
                 solver=self.solver,
                 alpha=self.alpha,
+                batch_size = self.batch_size,
+                learning_rate = self.learning_rate, 
                 learning_rate_init=self.learning_rate_init,
                 max_iter=self.max_iterations, 
+                tol = self.tol,
+                momentum = self.momentum,
+                nesterovs_momentum = self.nesterovs_momentum,
+                validation_fraction = self.validation_fraction,
+                beta_1 = self.beta_1,
+                beta_2 = self.beta_2,
+                epsilon = self.epsilon,
+                n_iter_no_change = self.n_iter_no_change,
                 random_state=self.seed,
                 early_stopping=self.early_stopping,
                 verbose=False
